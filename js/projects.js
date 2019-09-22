f04a9afe908cc4546754c7798250e9cb2e950b65
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-09-22 20:32:00 +0100

    Finally figured it out.

--- js/projects.js
+++ js/projects.js
@@ -1,28 +1,13 @@
-function spanContent(content) {
-  console.log(content);
-  if(content[content.length-1] == '\n');
-    content = content.slice(0,-1);
-  return content
-}
-
-function changeProjectFunc() {
+function changeProjectFunc(content) {
   var projects = ['foldingathome', 'BritBot'];
-
-  projectSpan = document.getElementById('project').value;
-  console.log(projectSpan);
-  content = spanContent(projectSpan);
+  console.log(content);
   if (projects.includes(content)) {
-    projectExampleLink = document.querySelectorAll('#projectExample');
-    projectCodeLink = document.querySelectorAll('#projectCode');
+    projectExampleLink = document.getElementById('projectExample');
+    projectCodeLink = document.getElementById('projectCode');
 
     projectExampleLink.href = `${content}Example.html`;
-    projectCodeLink.href = `${content}Code.html`;
+    projectExampleLink.style.textDecoration = 'underline';
+    projectCodeLink.href = `https://github.com/thenamesweretakenalready/${content}`;
+    projectCodeLink.style.textDecoration = 'underline'
   }
 }
-
-function setup() {
-  var content = document.getElementById('project');
-  content.addEventListener('keydown', changeProjectFunc());
-}
-
-setup();
