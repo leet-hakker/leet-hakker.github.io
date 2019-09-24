2beb21d8d452fbf891cce20e9cfbb21602a824bb
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-09-24 20:20:31 +0100

    Made links more obvious and pretty. You're welcome.

--- js/projects.js
+++ js/projects.js
@@ -1,22 +1,62 @@
 function changeProjectFunc(content) {
   var projects = ['foldingathome', 'BritBot'];
-  console.log(content);
   if (projects.includes(content)) {
     projectExampleLink = document.getElementById('projectExample');
     projectCodeLink = document.getElementById('projectCode');
 
     projectExampleLink.href = `${content}Example.html`;
-    projectExampleLink.style.textDecoration = 'underline';
+    projectExampleLink.classList.add('var-link');
     projectCodeLink.href = `https://github.com/thenamesweretakenalready/${content}`;
-    projectCodeLink.style.textDecoration = 'underline'
+    projectCodeLink.classList.add('var-link');
   }
   else {
     projectExampleLink = document.getElementById('projectExample');
     projectCodeLink = document.getElementById('projectCode');
 
     projectExampleLink.href = '#';
-    projectExampleLink.style.textDecoration = 'none';
+    projectExampleLink.classList.remove('var-link');
     projectCodeLink.href = '#';
-    projectCodeLink.style.textDecoration = 'none'
+    projectCodeLink.classList.remove('var-link');
   }
 }
+
+function linkTimeoutPulse() {
+  funcs = document.querySelectorAll('.func-link')
+  for (var i = 0; i < funcs.length; i++){
+    funcs[i].classList.add('func-underline');
+  }
+  strs = document.querySelectorAll('.str-link')
+  for (var i = 0; i < strs.length; i++){
+    strs[i].classList.add('str-underline');
+  }
+  vars = document.querySelectorAll('.var-link')
+  for (var i = 0; i < vars.length; i++){
+    vars[i].classList.add('var-underline');
+  }
+}
+
+function LinkClearTimeout(timeout) {
+
+  funcs = document.querySelectorAll('.func-link')
+  for (var i = 0; i < funcs.length; i++){
+    funcs[i].classList.remove('func-underline');
+  }
+  strs = document.querySelectorAll('.str-link')
+  for (var i = 0; i < strs.length; i++){
+    strs[i].classList.remove('str-underline');
+  }
+  vars = document.querySelectorAll('.var-link')
+  for (var i = 0; i < vars.length; i++){
+    vars[i].classList.remove('var-underline');
+  }
+  timeout = setTimeout(linkTimeoutPulse, 2000);
+}
+
+var timeout;
+
+
+timeout = setTimeout(linkTimeoutPulse, 2000);
+document.addEventListener('mousemove', function() {
+  clearTimeout(timeout);
+  LinkClearTimeout(timeout);
+});
